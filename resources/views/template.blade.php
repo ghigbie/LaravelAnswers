59dd6fdadb5917a877a138d26082da0c7820ee60
ghigbie <gehigbie@gmail.com>
2018-11-17 18:32:20 -0700

    Add new navabar template

--- resources/views/template.blade.php
+++ resources/views/template.blade.php
@@ -15,6 +15,8 @@
 </head>
 <body>
 
+    @yield('navbar')
+
     <nav class="navbar navbar-expand-lg navbar-light bg-light">
         <a class="navbar-brand inactive href="#">Laravel Answers</a>
         <button class="navbar-toggler collasped" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">

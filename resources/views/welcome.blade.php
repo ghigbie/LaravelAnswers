e72ccf4d0c7c377901801e6bc590d5b67f133e2e
ghigbie <gehigbie@gmail.com>
2018-11-17 18:14:45 -0700

    Add navbar

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -15,19 +15,35 @@
     </head>
     <body>
         <div class="flex-center position-ref full-height">
-            @if (Route::has('login'))
-                <div class="top-right links">
-                    @auth
-                        <a href="{{ url('/home') }}">Home</a>
-                    @else
-                        <a href="{{ route('login') }}">Login</a>
 
-                        @if (Route::has('register'))
-                            <a href="{{ route('register') }}">Register</a>
-                        @endif
-                    @endauth
+            <nav class="navbar navbar-expand-lg navbar-light bg-light">
+                <a class="navbar-brand inactive href="#">Laravel Answers</a>
+                <button class="navbar-toggler collasped" type="button" data-toggle="collapse" data-target="#navbarSupportedContent" aria-controls="navbarSupportedContent" aria-expanded="false" aria-label="Toggle navigation">
+                    <span class="navbar-toggler-icon"></span>
+                    <span class="sr-only">Toggle navigation</span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                    <span class="icon-bar"></span>
+                </button>
+
+                <div class="collapse navbar-collapse" id="navbarSupportedContent">
+                    <ul class="navbar-nav mr-auto">
+                        <li class="nav-item">
+                            <a class="nav-link" href="{{ route('index') }}">Home <span class="sr-only">(current)</span></a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Recent</a>
+                        </li>
+                        <li class="nav-item">
+                            <a class="nav-link" href="#">Popular</a>
+                        </li>
+                    </ul>
+                    <ul class="nav navbar-nav navbar-right">
+                        <a href="#" class="btn btn-outline-primary" style="margin-top:5px;">Ask A Question</a>
+                    </ul>
                 </div>
-            @endif
+            </nav>
+
 
             <div class="container">
                 <div class="jumbotron">
